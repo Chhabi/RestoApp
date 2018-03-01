ad4775a43e537ad5d5c6dfb7d11798c184a34c33
Romain Couperier <romain.couperier@mail.mcgill.ca>
2018-03-01 17:07:40 -0500

    Create updateTable function

--- src/ca/mcgill/ecse223/resto/controller/RestoAppController.java
+++ src/ca/mcgill/ecse223/resto/controller/RestoAppController.java
@@ -1,19 +1,9 @@
 package ca.mcgill.ecse223.resto.controller;
 
-import java.sql.Date;
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 import ca.mcgill.ecse223.resto.application.RestoAppApplication;
-import ca.mcgill.ecse223.resto.model.Bill;
-import ca.mcgill.ecse223.resto.model.Menu;
-import ca.mcgill.ecse223.resto.model.MenuItem;
-import ca.mcgill.ecse223.resto.model.MenuItem.ItemCategory;
 import ca.mcgill.ecse223.resto.model.Order;
-import ca.mcgill.ecse223.resto.model.OrderItem;
-import ca.mcgill.ecse223.resto.model.PricedMenuItem;
-import ca.mcgill.ecse223.resto.model.Reservation;
 import ca.mcgill.ecse223.resto.model.RestoApp;
 import ca.mcgill.ecse223.resto.model.Seat;
 import ca.mcgill.ecse223.resto.model.Table;
@@ -24,29 +14,87 @@ public class RestoAppController {
 	public RestoAppController() {
 	}
 	
-	//code for create/delete operation should look something like this
-//	public static void createTable(int number, int x, int y, int width, int length, int numberOfSeats) throws InvalidInputException {
-//		RestoApp restoApp = RestoAppApplication.getRestoApp();
-//		try {
-//			restoApp.addTable(number, x, y, width, length);
-//			//num seats?
-//			RestoAppApplication.save();
-//		}
-//		catch (RuntimeException e) {
-//			throw new InvalidInputException(e.getMessage());
-//		}
-//	}
+	public static void removeTable(int number) throws InvalidInputException {
+		RestoApp restoApp = RestoAppApplication.getRestoapp();
+		Table table = Table.getWithNumber(number);
+				
+		boolean reserved = table.hasReservations();
+		if(reserved == true) throw new InvalidInputException("Table is reserved");
+		try {
+			List<Order> currentOrders = restoApp.getCurrentOrders();
+			for(Order order : currentOrders) { //if table in use throw exception
+				List<Table> tables = order.getTables();
+				boolean inUse = tables.contains(table);
+				if(inUse == true) throw new InvalidInputException("Table is in use");
+			}
+			restoApp.removeCurrentTable(table);
+			RestoAppApplication.save();
+		}catch (RuntimeException e){
+			throw new InvalidInputException(e.getMessage());
+		}
+	}
 	
+	public static void removeTable(Table table) throws InvalidInputException {
+		if(table == null) throw new InvalidInputException("Invalid Table");
+		RestoApp restoApp = RestoAppApplication.getRestoapp();
+		boolean reserved = table.hasReservations();
+		if(reserved == true) throw new InvalidInputException("Table is reserved"); //if table reserved throw exception
+		try {
+			List<Order> currentOrders = restoApp.getCurrentOrders();
+			for(Order order : currentOrders) { //if table in use throw exception
+				List<Table> tables = order.getTables();
+				boolean inUse = tables.contains(table);
+				if(inUse == true) throw new InvalidInputException("Table is in use");
+			}
+			restoApp.removeCurrentTable(table);
+			RestoAppApplication.save();
+		}catch (RuntimeException e){
+			throw new InvalidInputException(e.getMessage());
+		}
+	}
 	
-//	//code for list should look something like this
-//	//JUST AN EXAMPLE
-//	public static List<ItemCategory> getItemCategories() {
-//		
-//		RestoApp restoApp = RestoAppApplication.getRestoApp();
-//		ArrayList<ItemCategory> result = new ArrayList<ItemCategory>();
-//		for (ItemCategory itemCategory : restoApp.getItemCategories()) {
-//			if (true) 
-//				result.add(itemCategory);
-//		}
-//		return result;
+	public static void updateTable(Table table, int newNumber, int numberOfSeats) throws InvalidInputException {
+		
+		if(table == null) {
+			throw new InvalidInputException("The table doesn't exist.");
+		} else if (newNumber < 0 || numberOfSeats < 0) {
+			throw new InvalidInputException("Numbers are not positive.");
+		} else if (table.hasReservations()) {
+			throw new InvalidInputException("The table is reserved.");
+		}
+		
+		RestoApp r = RestoAppApplication.getRestoapp();
+		
+		List<Order> currentOrders = r.getCurrentOrders();
+		
+		for(int k = 0; k < currentOrders.size(); k++) {
+			Order order = currentOrders.get(k);
+			List<Table> tables = order.getTables();
+			boolean inUse = tables.contains(table);
+			if (inUse) {
+				throw new InvalidInputException("Table in use.");
+			}
+		}
+		
+		try {
+			table.setNumber(newNumber);
+		} catch (RuntimeException e) {
+			throw new InvalidInputException("The new number is a duplicate.");
+		}
+		
+		int n = table.numberOfCurrentSeats();
+		
+		for (int k = 1; k < (numberOfSeats - n); k++) {
+			Seat seat = table.addSeat();
+			table.addCurrentSeat(seat);
+		}
+		
+		for (int k = 1; k < (n - numberOfSeats); k++) {
+			Seat seat = table.getCurrentSeat(0);
+			table.removeCurrentSeat(seat);
+		}
+		
+		RestoAppApplication.save();
+		
+	}
 }
